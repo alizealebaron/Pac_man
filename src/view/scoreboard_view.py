9c24bda400caa5e7f663b3b543321d3d943b35e8
alebaron <alizea.lebaron2@gmail.com>
2026-05-23 13:50:56 +0200

    620 (Modif): V1 du leaderboard (manque le fond)

--- src/view/scoreboard_view.py
+++ src/view/scoreboard_view.py
@@ -6,7 +6,7 @@
 #  By: alebaron, rruiz                           +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/23 10:58:55 by alebaron        #+#    #+#               #
-#  Updated: 2026/05/23 12:13:58 by alebaron        ###   ########.fr        #
+#  Updated: 2026/05/23 13:49:28 by alebaron        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -82,6 +82,9 @@ class ScoreboardView(arcade.View):
                              height)
         )
 
+        # Affichage des scores
+        self.draw_big_scoreboard()
+
     def on_mouse_press(self, x, y, _, __):
 
         print(f"{x},{y}")
@@ -89,3 +92,62 @@ class ScoreboardView(arcade.View):
         if (x > 2 and x < 95 and y > 995 and y < 1080):
             self.music.stop(self.music_player)
             self.window.show_view(self.window.start_view)
+
+    # +---------------------------------------------------------------------+
+    # |                            Draw Methods                             |
+    # +---------------------------------------------------------------------+
+
+    def draw_big_scoreboard(self):
+
+        # Tri des 3 meilleurs
+        scores = sorted(self.lst_score,
+                        key=lambda p: p.score,
+                        reverse=True)[:12]
+
+        # Configuration des positions
+        start_x = self.window.height / 2
+        start_y = (self.window.width / 2) - 80
+        line_height = 70
+        icon_size = 50
+
+        # Joueurs présents au top 3
+        for i, player in enumerate(scores):
+
+            current_y = start_y - (i * line_height)
+
+            # Image de rang
+            if (i < 12):
+                rank_tex = arcade.load_texture(f"assets/rank/rank_{i+1}_64.png")
+            else:
+                rank_tex = arcade.load_texture("assets/rank/rank_0.png")
+            arcade.draw_texture_rect(
+                texture=rank_tex,
+                rect=arcade.XYWH(start_x + (icon_size / 2), current_y,
+                                 icon_size, icon_size)
+            )
+
+            # Image du pokémon
+            pokemon = player.pokemon
+            profile_tex = arcade.load_texture(f"assets/sprite/pokemon/{pokemon}/portraits/Normal.png") 
+            arcade.draw_texture_rect(
+                texture=profile_tex,
+                rect=arcade.XYWH(start_x + icon_size + 40, current_y,
+                                 icon_size, icon_size)
+            )
+
+            sprite_frame = arcade.load_texture("assets/sprite/face_frame.png")
+            arcade.draw_texture_rect(
+                texture=sprite_frame,
+                rect=arcade.XYWH(start_x + icon_size + 40, current_y,
+                                 icon_size + 5, icon_size + 5)
+            )
+
+            # Nom + Score
+            text_content = f"{player.name} ({player.score})"
+            arcade.draw_text(
+                text_content,
+                start_x + (icon_size * 2) + 30,
+                current_y - 8,
+                color=arcade.color.BLACK,
+                font_size=16
+            )
