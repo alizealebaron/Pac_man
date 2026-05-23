c3d01a0ffdc59f9d6c3dd4ac0b8ac3ec405bf282
alebaron <alizea.lebaron2@gmail.com>
2026-05-23 12:16:21 +0200

    620 (Ajout): Début d'écran de scoreboard

---
+++ src/view/scoreboard_view.py
@@ -0,0 +1,91 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  scoreboard_view.py                                :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: alebaron, rruiz                           +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/05/23 10:58:55 by alebaron        #+#    #+#               #
+#  Updated: 2026/05/23 12:13:58 by alebaron        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+# +-------------------------------------------------------------------------+
+# |                               Importation                               |
+# +-------------------------------------------------------------------------+
+
+import arcade
+from src.models.scoreModel import Score
+
+# +-------------------------------------------------------------------------+
+# |                                 CONST                                   |
+# +-------------------------------------------------------------------------+
+
+
+BACKGROUND_PATH = "assets/background/score_background.png"
+
+
+# +-------------------------------------------------------------------------+
+# |                                 Classe                                  |
+# +-------------------------------------------------------------------------+
+
+class ScoreboardView(arcade.View):
+
+    # +---------------------------------------------------------------------+
+    # |                                Init                                 |
+    # +---------------------------------------------------------------------+
+
+    def __init__(self, window):
+
+        super().__init__(window)
+        self.background = arcade.load_texture(BACKGROUND_PATH)
+        self.lst_score = self.window.manager.scoreboard
+
+        # Initialisation de la musique
+        self.music_player = None
+
+    # +---------------------------------------------------------------------+
+    # |                            View Methods                             |
+    # +---------------------------------------------------------------------+
+
+    def on_show_view(self):
+        """Appelé quand la vue change"""
+        if not (self.music_player and self.music_player.playing):
+            self.music = arcade.Sound("assets/music/scoreboard_theme.mp3",
+                                      streaming=True)
+            self.music_player = self.music.play(volume=1, loop=True)
+
+    def on_draw(self):
+        """ Draw everything """
+        self.clear()
+
+        # Affichage du background
+        arcade.draw_texture_rect(
+            texture=self.background,
+            rect=arcade.XYWH(
+                self.window.width / 2,
+                self.window.height / 2,
+                self.window.width,
+                self.window.height
+            )
+        )
+
+        # Affichage du bouton retour
+        retour_sprite = arcade.load_texture("assets/button/retour.png")
+        height = 90
+        width = 170
+        arcade.draw_texture_rect(
+            texture=retour_sprite,
+            rect=arcade.XYWH(45,
+                             (self.window.height) - (height / 2),
+                             width,
+                             height)
+        )
+
+    def on_mouse_press(self, x, y, _, __):
+
+        print(f"{x},{y}")
+        # Bouton retour
+        if (x > 2 and x < 95 and y > 995 and y < 1080):
+            self.music.stop(self.music_player)
+            self.window.show_view(self.window.start_view)
